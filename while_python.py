d178e3fea688c7a7e1f7bd719856554c4d6b50a2
miki <mikinagash1997@gmail.com>
2022-03-09 19:18:13 +0200

    completed the 4 q

--- while_python.py
+++ while_python.py
@@ -13,11 +13,15 @@
 #      x = x +1
 
 #4
+
 # x = int(input("enter number:"))
 # status = int(input("print even number or not even number ? 1/2:"))
 # while x <= 20:
 #     if status == 1 and x % 2 ==1:
-#         print(x, "not even number  ")
+#      print(x, "not even number  ")
 #     elif status == 2 and x % 2 == 0:
-#         print(x, "enter number")
+#          print(x, "enter number")
 #     x = x +1
+# if  status > 2:
+#       print("error")
+
